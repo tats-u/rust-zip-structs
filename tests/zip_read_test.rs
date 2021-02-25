962dab01da9d9d743db2877b1040d5630eceffae
Tatsunori Uchino <tats.u@live.jp>
2021-02-25 21:02:36 +0900

    Use static variable to define UTF-8 literals

--- tests/zip_read_test.rs
+++ tests/zip_read_test.rs
@@ -4,6 +4,9 @@ use zip_structs::zip_local_file_header::ZipLocalFileHeader;
 mod test_util;
 use test_util::*;
 
+static EXPECTED_FILE_NAME_UTF8: &[u8] = "テスト.txt".as_bytes();
+static EXPECTED_FILE_CONTENT: &[u8] = "テスト".as_bytes();
+
 #[test]
 fn find_eocd_test() -> anyhow::Result<()> {
     let mut zip_file = open_assets_file("explicit_utf-8.zip")?;
@@ -37,13 +40,12 @@ fn eocd_cd_chain_read_test() -> anyhow::Result<()> {
 
     assert_eq!(cd_list.len(), 1);
     let cd = cd_list.pop().unwrap();
-    let file_name = String::from_utf8_lossy(&cd.file_name_raw);
-    assert_eq!(file_name, "テスト.txt");
+    assert_eq!(&cd.file_name_raw, EXPECTED_FILE_NAME_UTF8);
 
     assert!(cd.is_encoded_in_utf8());
     assert_eq!(cd.local_header_position, 0);
     assert_eq!(cd.disk_number_start, 0);
-    assert_eq!(file_name.len(), cd.file_name_length as usize);
+    assert_eq!(cd.file_name_raw.len(), cd.file_name_length as usize);
 
     Ok(())
 }
@@ -57,27 +59,24 @@ fn eocd_cd_lf_chain_read_test() -> anyhow::Result<()> {
     let cd = cd_list.pop().unwrap();
     let local_file_header = ZipLocalFileHeader::from_central_directory(&mut zip_file, &cd)?;
 
-    let file_name = String::from_utf8_lossy(&local_file_header.file_name_raw);
-    assert_eq!(file_name, "テスト.txt");
+    assert_eq!(&local_file_header.file_name_raw, EXPECTED_FILE_NAME_UTF8);
 
     assert_eq!(local_file_header.starting_position_with_signature, 0);
     assert_eq!(local_file_header.starting_position_without_signature, 4);
-
-    let expected_content = "テスト";
 
     // 0 = no compression
     assert_eq!(local_file_header.compression_method, 0);
     assert_eq!(
         local_file_header.compressed_size as usize,
-        expected_content.len()
+        EXPECTED_FILE_CONTENT.len()
     );
     assert_eq!(
         local_file_header.uncompressed_size as usize,
-        expected_content.len()
+        EXPECTED_FILE_CONTENT.len()
     );
     assert_eq!(
-        String::from_utf8_lossy(&local_file_header.compressed_data),
-        expected_content
+        local_file_header.compressed_data.as_ref(),
+        EXPECTED_FILE_CONTENT
     );
 
     Ok(())
@@ -92,9 +91,10 @@ fn legacy_filename_encoding_zip_read_test() -> anyhow::Result<()> {
     assert_eq!(cd_list.len(), 1);
     let cd = cd_list.pop().unwrap();
     // テスト.txt in Shift-JIS
-    let expected_file_name = [0x83, 0x65, 0x83, 0x58, 0x83, 0x67, 0x2e, 0x74, 0x78, 0x74];
-    assert_eq!(cd.file_name_length as usize, expected_file_name.len());
-    assert_eq!(&cd.file_name_raw, &expected_file_name);
+    static EXPECTED_FILE_NAME_SJIS: &[u8] =
+        &[0x83, 0x65, 0x83, 0x58, 0x83, 0x67, 0x2e, 0x74, 0x78, 0x74];
+    assert_eq!(cd.file_name_length as usize, EXPECTED_FILE_NAME_SJIS.len());
+    assert_eq!(&cd.file_name_raw, EXPECTED_FILE_NAME_SJIS);
 
     assert!(!cd.is_encoded_in_utf8());
     assert_eq!(cd.local_header_position, 0);
