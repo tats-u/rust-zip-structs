51d3d5e9033d9464cb808fe3f13b99f64bc5f1d2
Tatsunori Uchino <tats.u@live.jp>
2021-07-25 14:47:32 +0900

    HOTFIX: fix test for WinZip-style AES encryption

--- tests/encrypted_zip.rs
+++ tests/encrypted_zip.rs
@@ -55,6 +55,11 @@ fn regular_encryption_comparing_test() -> anyhow::Result<()> {
     return Ok(());
 }
 
+/// WinZip-style AES encryption
+///
+/// https://www.winzip.com/win/en/aes_info.html
+///
+/// 7-Zip adopts this format
 #[test]
 fn aes_encryption_comparing_test() -> anyhow::Result<()> {
     let mut sjis_zip = open_assets_file("aes256_sjis.zip")?;
@@ -78,16 +83,22 @@ fn aes_encryption_comparing_test() -> anyhow::Result<()> {
         sjis_cd.version_required_to_extract,
         utf8_cd.version_required_to_extract
     );
-    assert_eq!(sjis_cd.compression_method, utf8_cd.compression_method);
-    assert_eq!(sjis_cd.crc32, utf8_cd.crc32);
+    // Compression method must be 99 (II. B.)
+    assert_eq!(sjis_cd.compression_method, 99);
+    assert_eq!(utf8_cd.compression_method, 99);
+    // CRC must be 0 (II. C.)
+    assert_eq!(sjis_cd.crc32, 0);
+    assert_eq!(utf8_cd.crc32, 0);
     assert_eq!(sjis_cd.is_encoded_in_utf8(), false);
     assert_eq!(utf8_cd.is_encoded_in_utf8(), true);
     assert_eq!(sjis_cd.is_encrypted_central_directory(), false);
-    assert_eq!(sjis_cd.is_encrypted_data(), true);
-    assert_eq!(sjis_cd.is_strongly_encrypted_data(), true);
     assert_eq!(utf8_cd.is_encrypted_central_directory(), false);
+    // bit #0 (encrypted file) must be true (II. B.)
+    assert_eq!(sjis_cd.is_encrypted_data(), true);
     assert_eq!(utf8_cd.is_encrypted_data(), true);
-    assert_eq!(utf8_cd.is_strongly_encrypted_data(), true);
+    // bit #6 (strong encryption) must be false
+    assert_eq!(sjis_cd.is_strongly_encrypted_data(), false);
+    assert_eq!(utf8_cd.is_strongly_encrypted_data(), false);
 
     let sjis_local_header = ZipLocalFileHeader::from_central_directory(&mut sjis_zip, &sjis_cd)?;
     let utf8_local_header = ZipLocalFileHeader::from_central_directory(&mut utf8_zip, &sjis_cd)?;
